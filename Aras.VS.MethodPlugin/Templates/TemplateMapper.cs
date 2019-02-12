e9855b8bcc68fd3fec3a455f7d0e778c8c9c8625
Volodymyr Afanasiev <s-vafanasiev@aras.com>
2019-02-12 16:02:09 +0200

    TemplateMapper has been extended by next templates: - "CSharp:Aras.Server.Core.GraphNavigation.PropertyGetter" to "GNPropGet" - "CSharp:Aras.Server.Core.GraphNavigation.NodeValuesGetter" to "GNNodeValGet" - "CSharp:Aras.Server.Core.GraphNavigation.KeyStringGetter" to "GNKeyStrGet"

--- Aras.VS.MethodPlugin/Templates/TemplateMapper.cs
+++ Aras.VS.MethodPlugin/Templates/TemplateMapper.cs
@@ -1,26 +1,36 @@
-﻿using System.Collections.Generic;
+﻿//------------------------------------------------------------------------------
+// <copyright file="TemplateMapper.cs" company="Aras Corporation">
+//     © 2017-2018 Aras Corporation. All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Collections.Generic;
 
 namespace Aras.VS.MethodPlugin.Templates
 {
-    public static class TemplateMapper
-    {
-        private static readonly Dictionary<string, string> templateNameMapper = new Dictionary<string, string>
-        {
-            {"CSharp", "CSharp"},
-            {"CSharpInOut", "CSharpIO"},
-            {"ConflictDetectionLocalRuleCSharp.version:1", "CDLocRule"},
-            {"ConflictDetectionLocalRuleSimplifiedCSharp.version:1", "CDLRSimpl"},
-            {"CSharp:Aras.Server.Core.AccessControl.EnvironmentAttributeMethod", "CoreAccess"},
-            {"CSharp:Aras.Server.Core.Configurator", "CoreConfig"},
-            {"CSharp:Aras.Server.Core.Configurator.ScopeStructureOutput", "CoreScope"},
-            {"CSharp:Aras.TDF.ContentGenerator(Extended)", "TDFExtend"},
-            {"CSharp:Aras.TDF.ContentGenerator(Strict)", "TDFStrict"},
-            {"CSharp:Aras.TreeGridView.BuilderMethod", "TreeGrid"}
-        };
+	public static class TemplateMapper
+	{
+		private static readonly Dictionary<string, string> templateNameMapper = new Dictionary<string, string>
+		{
+			{"CSharp", "CSharp"},
+			{"CSharpInOut", "CSharpIO"},
+			{"ConflictDetectionLocalRuleCSharp.version:1", "CDLocRule"},
+			{"ConflictDetectionLocalRuleSimplifiedCSharp.version:1", "CDLRSimpl"},
+			{"CSharp:Aras.Server.Core.AccessControl.EnvironmentAttributeMethod", "CoreAccess"},
+			{"CSharp:Aras.Server.Core.Configurator", "CoreConfig"},
+			{"CSharp:Aras.Server.Core.Configurator.ScopeStructureOutput", "CoreScope"},
+			{"CSharp:Aras.TDF.ContentGenerator(Extended)", "TDFExtend"},
+			{"CSharp:Aras.TDF.ContentGenerator(Strict)", "TDFStrict"},
+			{"CSharp:Aras.TreeGridView.BuilderMethod", "TreeGrid"},
+			{"CSharp:Aras.Server.Core.GraphNavigation.PropertyGetter", "GNPropGet"},
+			{"CSharp:Aras.Server.Core.GraphNavigation.NodeValuesGetter", "GNNodeValGet"},
+			{"CSharp:Aras.Server.Core.GraphNavigation.KeyStringGetter", "GNKeyStrGet"}
+		};
 
-        public static string GetAliasTemplateName(string fullName)
-        {
-            return templateNameMapper[fullName];}
+		public static string GetAliasTemplateName(string fullName)
+		{
+			return templateNameMapper[fullName];
+		}
 
-    }
+	}
 }
