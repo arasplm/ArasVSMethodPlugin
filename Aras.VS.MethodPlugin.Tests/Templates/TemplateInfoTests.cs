3927ab250c5b4d741edebf25d89fa563d305a6ee
Volodymyr Afanasiev <s-vafanasiev@aras.com>
2019-04-01 12:08:46 +0300

    Added tests to 'TemplateInfo'

---
+++ Aras.VS.MethodPlugin.Tests/Templates/TemplateInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using Aras.VS.MethodPlugin.Templates;
+using NUnit.Framework;
+
+namespace Aras.VS.MethodPlugin.Tests.Templates
+{
+	[TestFixture]
+	public class TemplateInfoTests
+	{
+		[TestCase("", "")]
+		[TestCase(null, null)]
+		[TestCase("TemplateName", "TemplateName")]
+
+		public void ToString_ShouldReturnExpectedValue(string templateName, string expected)
+		{
+			// Arrange
+			TemplateInfo templateInfo = new TemplateInfo
+			{
+				TemplateName = templateName
+			};
+
+			// Act
+			string actual = templateInfo.ToString();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
